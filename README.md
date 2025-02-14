83448d226f1ed1f426415cca504242f775630c4a
Anton Älgmyr <anton@algmyr.se>
2025-02-14 23:51:58 +0100

    Minimal restricted and hacky version.

---
+++ README.md
@@ -0,0 +1,11 @@
+# G'MIC tree-sitter grammar.
+
+> [!CAUTION]
+> This is a very bare-bones attempt at making a parser based on a single G'MIC
+> file I wrote. I'm neither very experiences in writing tree-sitter grammars
+> nor super experiences in G'MIC particularities.
+
+Very hacky, very incomplete. Did well enough on the one file I care about.
+Maybe something that could be generalized by people who know the language.
+
+Also, really needs tests.
--- grammar.js
+++ grammar.js
@@ -7,11 +7,107 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
+identifier = /[A-Za-z_][A-Za-z_0-9]*/
+
 module.exports = grammar({
   name: "gmic",
 
+  conflicts: $ => [
+    [$.command],
+  ],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => repeat(choice($.command, $.comment)),
+
+    comment: _ => token(prec(-10, /#[^\n]*/)),
+
+    command: $ => seq(
+      alias($.identifier, $.command_name_def), ":",
+      repeat(choice($.statement, $.comment)),
+    ),
+
+    statement: $ => choice(
+      $.assignment,
+      $.if_statement,
+      $.command_call,
+    ),
+
+    command_call: $ => seq(
+      optional("+"),
+      alias($.identifier, $.command_name),
+      optional($.indexing),
+      repeat($.array_expression),
+      $.terminator,
+    ),
+
+    indexing: $ => seq(token.immediate("["), $.expression, "]"),
+
+    if_statement: $ => seq(
+      "if", alias($.string, $.condition), repeat($.statement),
+      optional(seq("elif", alias($.string, $.condition), repeat($.statement))),
+      optional(seq("else", repeat($.statement))),
+      "fi"
+    ),
+
+    expression: $ => seq(
+      optional(/[\-+]/),
+      choice(
+        $.integer,
+        $.variable,
+        $.math_environment,
+        $.string,
+        $.bare_string,
+      ),
+    ),
+
+
+    math_environment: $ => seq(
+      "{", 
+        choice(
+          $.math_expression,
+          $.string,
+        ),
+      "}"
+    ),
+
+    // Super naive.
+    math_expression: $ => choice(
+      $.integer,
+      $.unary_expression,
+      $.binary_expression,
+      $.identifier,  // Built-in
+      $.variable,
+    ),
+
+    integer: $ => /\d+/,
+    // Naive string.
+    string: $ => /"[^"]*"/,
+    
+    unary_expression: $ => seq(/[+\-]/, $.math_expression),
+    binary_expression: $ => choice(
+      prec.left(2, seq($.math_expression, /[*/]/, $.math_expression)),
+      prec.left(1, seq($.math_expression, /[\-+]/, $.math_expression)),
+    ),
+
+    variable: $ => choice(
+        seq("$", identifier),
+        /\$[0-9]/,
+    ),
+
+
+    assignment: $ => seq(
+      $.identifier, "=", $.array_expression,
+    ),
+
+    array_expression: $ => seq(
+      $.expression, repeat(seq(",", $.expression)),
+    ),
+
+    identifier: $ => identifier,
+
+    terminator : $ => "\n",
+
+    // Bare string, should be low priority.
+    bare_string: $ => /[/.a-zA-Z_\d][/.a-zA-Z_\d\-]*/,
   }
 });
